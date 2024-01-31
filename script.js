7e6d15c15322bf16d8b6e626a7680c7b9751349d
Caetano Labaki <caetanolz@hotmail.com>
2024-01-31 15:26:28 -0300

    Correção do Projeto
    Inclui o operador de comparação "ou" (||) para tornar o código mais legível e curto.

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 function playRockPaperScissor(player1, player2) {
-let resultado  = "";
-if (player1 == player2) {
-    resultado = "Empate!";
-}
-else if (player1 === "Pedra" && player2 === "Papel") {
-    resultado = "Jogador 2 venceu!";
-}
-else if (player1 === "Pedra" && player2 === "Tesoura") {
-    resultado = "Jogador 1 venceu!";
-}    
-else if (player1 === "Papel" && player2 === "Pedra") {
-    resultado = "Jogador 1 venceu!";
-}
-else if (player1 === "Papel" && player2 === "Tesoura") {
-    resultado = "Jogador 2 venceu!";
-}
-else if (player1 === "Tesoura" && player2 === "Pedra") {
-    resultado = "Jogador 2 venceu!";
-}
-else if (player1 === "Tesoura" && player2 === "Papel") {
-    resultado = "Jogador 1 venceu!";
-}
-return resultado;
-}
-console.log(playRockPaperScissor("Pedra","Tesoura"));
+    let resultado  = "";
+    if (player1 == player2) {
+        resultado = "Empate!";
+    }
+    else if ((player1 === "Pedra" && player2 === "Papel") || (player1 === "Papel" && player2 === "Tesoura") || (player1 === "Tesoura" && player2 === "Pedra")) {
+        resultado = "Jogador 2 venceu!";
+    }
+    else if ((player1 === "Pedra" && player2 === "Tesoura") || (player1 === "Papel" && player2 === "Pedra") || (player1 === "Tesoura" && player2 === "Papel")) {
+        resultado = "Jogador 1 venceu!";
+    }    
+    return resultado;
+    }
+    console.log(playRockPaperScissor("Pedra","Tesoura"));
 
